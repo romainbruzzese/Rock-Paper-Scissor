353b9f122a97b4c81211184f82b3178175df13ff
romainbruzzese <46422507+romainbruzzese@users.noreply.github.com>
2019-01-09 19:20:03 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Romain Bruzzese - Broken Rock Paper Scissors</title>
+<?php require_once "bootstrap.php"; ?>
+</head>
+<body>
+<div class="container">
+<h1>Welcome to Broken Rock Paper Scissors</h1>
+<p>
+<strong><a href="login.php"> => Please Log In <= </a></strong>
+</p>
+<p>
+Attempt to go to 
+<a href="game.php">game.php</a> without logging in - it should fail with an error message.
+<p>
+<a href="http://www.wa4e.com/code/rps.zip"
+ target="_blank">Source Code for this Application</a>
+</p>
+</div>
+</body>
+
